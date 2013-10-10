b6a58489a16bdd7ea45f3459dfb83907f542b64f
Apprentice <jvanstry@gmail.com>
2013-10-10 15:16:58 -0500

    dart model and specs

--- app/models/dart.rb
+++ app/models/dart.rb
@@ -1,2 +1,13 @@
 class Dart < ActiveRecord::Base
+  has_many :dartboards
+  has_many :boards, through: :dartboards
+
+  belongs_to :user
+
+  valid_url_suffix = /\.(?:png|jpg|jpeg|gif|mp4)/i
+
+  validates :image_url, presence: true, format: { with: valid_url_suffix }
+  validates :link_url, presence: true
+  validates :user_id, presence: true
+
 end
