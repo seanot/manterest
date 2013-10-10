de2926834f3659cf5074d48ba37d9f9f66568aab
Apprentice <jvanstry@gmail.com>
2013-10-10 16:17:41 -0500

    dartboard model tests complete

--- spec/models/dartboard_spec.rb
+++ spec/models/dartboard_spec.rb
@@ -1,5 +1,27 @@
 require 'spec_helper'
 
 describe Dartboard do
-  pending "add some examples to (or delete) #{__FILE__}"
+
+  before do
+    @dartboard = Dartboard.new(:caption => "sumshit",
+                               :board_id => "1",
+                               :dart_id => "1")
+  end
+
+  subject { @dartboard }
+
+  describe "when a caption is not present" do
+    before { @dartboard.caption = nil }
+    it { should_not be_valid }
+  end
+
+  describe "when a board_id is not present" do
+    before { @dartboard.board_id = nil }
+    it { should_not be_valid }
+  end
+
+  describe "when a dart_id is not present" do
+    before { @dartboard.dart_id = nil }
+    it { should_not be_valid }
+  end
 end
