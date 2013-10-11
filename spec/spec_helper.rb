c267d6af5285ac8bf26cb1c93aaeabb5da49032b
dbc-apprentice <apprentice@devbootcamp.com>
2013-10-11 15:50:53 -0500

    added travis and coveralls

--- spec/spec_helper.rb
+++ spec/spec_helper.rb
@@ -4,6 +4,8 @@ require File.expand_path("../../config/environment", __FILE__)
 require 'rspec/rails'
 require 'rspec/autorun'
 
+
+
 # Requires supporting ruby files with custom matchers and macros, etc,
 # in spec/support/ and its subdirectories.
 Dir[Rails.root.join("spec/support/**/*.rb")].each { |f| require f }
@@ -40,4 +42,7 @@ RSpec.configure do |config|
   # the seed, which is printed after each run.
   #     --seed 1234
   config.order = "random"
+
+  require 'coveralls'
+  Coveralls.wear!
 end
